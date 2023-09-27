6cc48265ddb9f9a35f629bfeb98cbc5b3e29c370
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-09-27 23:31:15 +0300

    clean code

--- client/found/found.view.ts
+++ client/found/found.view.ts
@@ -20,31 +20,36 @@ namespace $.$$ {
 			return this.founds().filter( ( f: any ) => f.uri && f.active !== true ).map( ( found: any ) => this.Found_new( found.uri ) ) || []
 		}
 
+		get_found(id: string) {
+			return this.founds().find( ( found: any ) => found.uri === id )
+		}
+
 		// Это нужно для view.tree - получаем наше поле объекта из массива
 		found_active_title( id: any ): string {
-			return this.founds().find( ( found: any ) => found.uri === id )?.title || ''
+			return this.get_found(id)?.title || ''
 		}
 		found_active_uri( id: any ): string {
-			return this.founds().find( ( found: any ) => found.uri === id )?.uri || ''
+			return this.get_found(id)?.uri || ''
 		}
 
 		found_new_title( id: any ): string {
-			return this.founds().find( ( found: any ) => found.uri === id )?.title || ''
+			return this.get_found(id)?.title || ''
 		}
 		found_new_uri( id: any ): string {
-			return this.founds().find( ( found: any ) => found.uri === id )?.uri || ''
+			return this.get_found(id)?.uri || ''
 		}
 
 		add_new_found( next?: any ) {
 			console.log( 'test', next )
 			if( this.new_found_title() && this.new_found_uri() ) {
 				this.add_new_found_fetch( this.new_found_title(), this.new_found_uri() )
+				this.founds()
 			}
 		}
 
 		add_new_found_fetch( title: string, uri: string ) {
 			const url = 'https://dosha-api-default-rtdb.firebaseio.com/founds.json'
-			const data = $mol_fetch.json( url, {
+			$mol_fetch.json( url, {
 				method: 'POST',
 				body: JSON.stringify(
 					{ title, uri }
