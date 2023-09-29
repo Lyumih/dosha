fa19a93b4dbfb62d07e820f76608f7edd3c5b492
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-09-30 01:08:57 +0300

    add auth move

--- client/auth/auth.view.ts
+++ client/auth/auth.view.ts
@@ -1,12 +1,19 @@
 namespace $.$$ {
 	export class $dosha_client_auth extends $.$dosha_client_auth {
-		
+
 		logout() {
+			$mol_state_arg.value('page', "login")
 			this.auth( false )
 		}
+
 		login() {
-			console.log('login')
+			$mol_state_arg.value('page', null)
 			this.auth( true )
+		}
+
+		pages() {
+			const page = $mol_state_arg.value('page')
+			return page === 'registration' ? [ this.Registration() ] : [ this.Login() ]
 		}
 
 		@$mol_mem
