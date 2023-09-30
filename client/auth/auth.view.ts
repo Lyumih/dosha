4a5e9f26582ceb57fe4a9aba1be8d77eea80f50d
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-09-30 04:22:51 +0300

    add repos

--- client/auth/auth.view.ts
+++ client/auth/auth.view.ts
@@ -1,19 +1,30 @@
 namespace $.$$ {
 	export class $dosha_client_auth extends $.$dosha_client_auth {
 		logout() {
-			$mol_state_arg.go({
+			$mol_state_arg.go( {
 				page: 'login'
-			})
+			} )
 			this.auth( false )
 		}
 
 		login() {
-			$mol_state_arg.go({})
-			this.auth( true )
+			console.log( 'login' )
+
+			const API_KEY = 'AIzaSyCbuQH3ZZnXCarsdaCGMW_Jrj4hzjcKsgQ'
+			// const url = 'https://dosha-api-default-rtdb.firebaseio.com/v1/accounts:signInWithCustomToken?key=AIzaSyCbuQH3ZZnXCarsdaCGMW_Jrj4hzjcKsgQ'
+			// const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`
+			// const url = `https://dosha-api-default-rtdb.firebaseio.com/v1/accounts:signUp?key=${ API_KEY }`
+			// const body = { email: "user@example.com", password: "[PASSWORD]", returnSecureToken: true }
+			const url = 'http://localhost:1337/api/founds'
+
+			const data = $mol_fetch.json( url ) as any | null
+			console.log( data, [ ...Object.values( data ) ] )
+			// $mol_state_arg.go({})
+			// this.auth( true )
 		}
 
 		pages() {
-			const page = $mol_state_arg.value('page')
+			const page = $mol_state_arg.value( 'page' )
 			return page === 'registration' ? [ this.Registration() ] : [ this.Login() ]
 		}
 
