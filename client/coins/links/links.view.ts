dc7272c58227643072d39102fa71db7ebde01d14
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-09-30 20:36:04 +0300

    fix

--- client/coins/links/links.view.ts
+++ client/coins/links/links.view.ts
@@ -1,47 +1,34 @@
-// namespace $ {
-// 	export let CoinsModel = $.$mol_data_record({
-		
-// 	})
-// }
-
 namespace $.$$ {
 	export class $dosha_client_coins_links extends $.$dosha_client_coins_links {
 		@ $mol_mem
-		fetch_coins() {
-			const result = this.$.$dosha_fetch_user.json('coins')
-			// @ts-ignore
-			return this.coins_result(result.data[0].attributes)
-		}
-
-		fetch_test() {
-			this.fetch_coins()
-		}
-
-		@ $mol_mem
 		coins_result(next?: any) {
-			return next ?? {}
+			return next ?? null
 		}
 
 		@ $mol_mem
 		steps() {
-			this.fetch_test()
-			return this.coins_result().steps || 0 + ' 👟';
+			if (this.coins_result()?.steps === undefined) {
+				// @ts-ignore
+				const result = this.$.$dosha_fetch_user.json('coins')?.data[0]?.attributes
+				this.coins_result(result ?? {steps: 0})
+			}
+			return this.coins_result()?.steps || 0 + ' 👟';
 		}
 		@ $mol_mem
 		training() {
-			return this.coins_result().training || 0 + ' 🏋';
+			return this.coins_result()?.training || 0 + ' 🏋';
 		}
 		@ $mol_mem
 		achievements() {
-			return this.coins_result().achievements || 0 + ' 🏆';
+			return this.coins_result()?.achievements || 0 + ' 🏆';
 		}
 		@ $mol_mem
 		goods() {
-			return this.coins_result().goods || 0 + ' 👑';
+			return this.coins_result()?.goods || 0 + ' 👑';
 		}
 		@ $mol_mem
 		charity() {
-			return this.coins_result().charity || 0 + ' 🏥';
+			return this.coins_result()?.charity || 0 + ' 🏥';
 		}
 	}
 }
