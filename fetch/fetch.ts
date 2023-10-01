7d11525a08940423f3aa88a6cb143b1393abb77e
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-01 16:59:23 +0300

    fix fetch

--- fetch/fetch.ts
+++ fetch/fetch.ts
@@ -2,7 +2,7 @@ namespace $ {
 	export class $dosha_fetch extends $mol_fetch {
 		static json( url: string, init?: RequestInit ) {
 			const develop_mode = $mol_state_arg.href_normal().startsWith( 'http://localhost' )
-			const prod_uri_db = 'https://2022831-koplenov.twc1.net/api/'
+			const prod_uri_db = 'https://2022831-koplenov.twc1.net//api/'
 			const local_uri_db = 'http://localhost:1337/api/'
 			const headers = {
 				Accept: 'application/json',
@@ -19,14 +19,14 @@ namespace $ {
 					...init?.headers
 				}
 			}
-			if( develop_mode ) {
-				return super.json( local_uri_db + url, initWithHeaders )
-			}
-			try {
+			// if( develop_mode ) {
+			// 	return super.json( local_uri_db + url, initWithHeaders )
+			// }
+			// try {
 				return super.json( ( develop_mode ? local_uri_db : prod_uri_db ) + url, initWithHeaders )
-			} catch {
-				return super.json( local_uri_db + url, initWithHeaders )
-			}
+			// } catch {
+			// 	return super.json( local_uri_db + url, initWithHeaders )
+			// }
 		}
 
 		static json_post( url: string, init?: RequestInit ) {
