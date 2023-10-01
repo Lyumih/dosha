815d0f24eeb140e2b5ea4cc2532c643834f5a629
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-01 09:08:07 +0300

    Пасхалки и брендинг страницы

---
+++ branding/branding.view.ts
@@ -0,0 +1,20 @@
+namespace $.$$ {
+	export class $dosha_branding extends $.$dosha_branding {
+		
+		config_theme() {
+			return 'config_theme';
+		}
+		config_background()	{
+			return 'config_background';
+		}
+
+		config_content() {
+			return 'config_content';
+		}
+
+		config_all(){
+			return 'config_all';
+		}
+		
+	}
+}
