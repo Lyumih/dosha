7c0bb7490945035378e8c0bb5829206524ca87d3
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-01 12:26:39 +0300

    update company pages

--- client/coins/coins.view.ts
+++ client/coins/coins.view.ts
@@ -1,9 +1,9 @@
 namespace $.$$ {
 	export class $dosha_client_coins extends $.$dosha_client_coins {
 		
-		// fetch_coins(){
-		// 	const result = this.$.$dosha_fetch.json('coins')
-		// }
+		fetch_coins(){
+			const result = this.$.$dosha_fetch.json('coins')
+		}
 		
 	}
 }
--- company/report/report.view.ts
+++ company/report/report.view.ts
@@ -2,7 +2,7 @@ namespace $.$$ {
 	export class $dosha_company_report extends $.$dosha_company_report {
 		
 		download() {
-			const report_content = 'Отчёт для оплаты. Контент'
+			const report_content = 'Отчёт компании по проекту Добрый Шаг'
 			return new $mol_dom_context.Blob( [ report_content ], { type: 'text/x-marked' } )
 
 		}
