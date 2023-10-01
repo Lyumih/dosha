92cc3a0d607ed0be16064fde0472a3c9cf9086df
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-01 16:50:43 +0300

    push fix

--- fetch/fetch.ts
+++ fetch/fetch.ts
@@ -2,7 +2,7 @@ namespace $ {
 	export class $dosha_fetch extends $mol_fetch {
 		static json( url: string, init?: RequestInit ) {
 			const develop_mode = $mol_state_arg.href_normal().startsWith( 'http://localhost' )
-			const prod_uri_db = 'https://2022831-koplenov.twc1.net//api/'
+			const prod_uri_db = 'https://2022831-koplenov.twc1.net/api/'
 			const local_uri_db = 'http://localhost:1337/api/'
 			const headers = {
 				Accept: 'application/json',
