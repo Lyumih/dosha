7e3378ac223c675ca1173e7a64950cb955010d18
Lyumih <Lyumih@users.noreply.github.com>
2023-12-18 07:15:33 +0000

    Deploying to gh-pages from @ Lyumih/dosha@f406ab234c12d36af1828053b2401193198c1a7a 🚀

--- web.d.ts
+++ web.d.ts
@@ -2028,6 +2028,7 @@ declare namespace $ {
         target(): string;
         file_name(): string;
         current(): boolean;
+        relation(): string;
         event_click(event?: any): any;
         click(event?: any): any;
     }
